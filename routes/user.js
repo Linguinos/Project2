ea2ceceffff96483a36d1a08c0ddf60b5a162a4b
Miki Blasco <blascom@7@gmail.com>
2022-02-19 19:21:45 +0100

    update routes (edit)

--- routes/user.js
+++ routes/user.js
@@ -25,10 +25,9 @@ router.get("/feed", isLoggedIn, (req, res) => {
 
 router.get("/profile", isLoggedIn, (req, res, next) => {
   const id = req.session.userId;
-  console.log("1", id);
   User.findById(id)
   .then(user => {
-    res.render("user/profile-user")
+    res.render("user/profile-user", user)
     //res.redirect("/")
     console.log(user)
   })
@@ -36,20 +35,23 @@ router.get("/profile", isLoggedIn, (req, res, next) => {
 });
 
 router.route("/edit")
-    .get(isLoggedIn, (req, res) => {
-      const id = req.session.userId; 
-      User.findById(id)
-      .then((user)=> res.render("user/profile-edit", user))
-      .post(isLoggedIn, (req, res)=>{
-         const {username, description, city, languageSpeak, languageLearn, imgUrl} = req.body
-         const user = req.session.userId
-         User.findByIdAndUpdate(id, {username, description, city, languageSpeak, languageLearn, imgUrl})
+.get(isLoggedIn, (req, res) => {
+  const id = req.session.userId; 
+  
+  User.findById(id)
+    .then((user)=> res.render("user/profile-edit", user))
+      
+})
+.post(isLoggedIn, (req, res)=>{
+  const {username, description, city, languageSpeak, languageLearn, imgUrl} = req.body
+  const id = req.session.userId;
+
+  User.findByIdAndUpdate(id, {username, description, city, languageSpeak, languageLearn, imgUrl})
             .then(()=>{
-                res.redirect("/user/profile-user");
+                res.redirect("/user/profile");
             })
             .catch(error=>{res.render("user/profile-edit")})
-            })
- });
+});
 
 module.exports = router;
 
