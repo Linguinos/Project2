e1f3812f6061977bb9da3014daab4bbfe6711835
Aleix Abulí <aleixabuli088@gmail.com>
2022-02-20 21:13:08 +0100

    populate y small refactoring

--- routes/meeting.js
+++ routes/meeting.js
@@ -15,6 +15,7 @@ router.get("/", isLoggedIn, (req, res) => {
     .catch(err=>console.log(err))
 });
 
+
 router.route('/create')
 .get(isLoggedIn, (req, res) => {
     res.render('meetings/meeting-create')
@@ -27,10 +28,12 @@ router.route('/create')
 
     User.findById(host)
     .then((user) => {
-        const hostId = user.id
-        Meeting.create({name, hostId, typeOfMeeting, language})
+        const host = user._id;
+
+        Meeting.create({name, host, typeOfMeeting, language})
         .then((meeting)=>{
             console.log("2222222222222222222", meeting);
+
             User.findByIdAndUpdate(req.session.userId, {$push: {meetings: meeting._id}})
             .then(() => {
                 res.redirect('/meetings');
@@ -38,23 +41,28 @@ router.route('/create')
         })
     })
     .catch(err=>console.log(err))
+});
 
-})
 
 router.get('/:id', isLoggedIn, (req, res) => {
     const id = req.params.id;
 
     Meeting.findById(id)
+    .populate('host')
     .then((meeting) => {
         res.render('meetings/meeting-details', meeting)
     })
-})
+    .catch(error => console.log(error));
+});
+
 
 router.post("/:id/delete", (req, res, next) => {
     const id = req.params.id;
     Meeting.findByIdAndDelete(id)
         .then(res.redirect("/meetings"))
-})
+        .catch(error => console.log(error));
+});
+
 
 router.route("/:id/edit")
 .get(isLoggedIn, (req, res) => {
@@ -62,17 +70,17 @@ router.route("/:id/edit")
   console.log(id)
   Meeting.findById(id)
     .then((meeting) => res.render("meetings/meeting-edit", meeting))
-      
+    .catch(error => console.log(error));
 })
 .post(isLoggedIn, (req, res)=>{
   const {name, typeOfMeeting, language} = req.body
   const id = req.params.id;
 
   Meeting.findByIdAndUpdate(id, {name, typeOfMeeting, language})
-            .then((meeting)=>{
-                res.render("meetings/meeting-details", meeting);
-            })
-            .catch(error=>{res.render("meetings/meeting-edit")})
+    .then((meeting)=>{
+        res.render("meetings/meeting-details", meeting);
+    })
+    .catch(error=>{res.render("meetings/meeting-edit")});
 });
 
 
