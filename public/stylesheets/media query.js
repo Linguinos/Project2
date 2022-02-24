dfd5c144fd826c95bf8ae51723fc1afe0ca5d986
AnaHerr <ana.herrera.91@gmail.com>
2022-02-24 14:12:10 +0100

    changes

---
+++ public/stylesheets/media query.js
@@ -0,0 +1,117 @@
+
+@media(min-width: 901px){
+    body {
+      font-size: 7px;
+      padding: 0px;
+      margin: 0px;
+      font-family: "Montserrat", sans-serif;
+    }
+    
+    a {
+      color: #00B7FF;
+      font-size: 1.3rem;
+    }
+    
+    p {
+      font-size: 1.3rem;
+    }
+    
+    h1 {
+      font-size: 2.2rem;
+    }
+    
+    h2 {
+      font-size: 1.8rem;
+    }
+    
+    h3 {
+      font-size: 1.6rem;
+    }
+    
+    
+    button {
+      padding: 7px;
+      background-color: #6ccff6;
+      border: 0px;
+      border-radius: 7px;
+      color: white;
+    }
+    
+    nav {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      position: relative;
+      width: 100%;
+      background-color: #1f2421;
+      
+      height: 60px; /* browser version */
+    }
+    
+    nav a {
+      color: white;
+      text-decoration: none;
+      font-size: 1.5rem;
+      font-weight: 600;
+    }
+    
+    nav h1 {
+      color: white;
+      font-size: 2.3rem;
+      font-weight: 800;
+    }
+    
+    .logo {
+      height: 50px;
+    }
+    
+    .logoTitle {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding-left: 0px;
+    }
+    
+    .logoTitle * {
+      padding-left: 0;
+      padding-right: 0px;
+    }
+    
+    .logout {
+      padding: 5px 12px;
+      border-radius: 10px;
+      background-color: #6ccff6;
+      color: #1f2421;
+    }
+    
+    /*.linkDiv {
+      display: flex;
+      width: 350px;
+      justify-content: space-between;
+      align-items: center;
+      padding-right: 10px;
+    }*/
+    
+    .linkDiv {
+      display: flex;
+      width: 50%;
+      justify-content: space-between;
+      align-items: center;
+      padding-right: 3%;
+    }
+    
+    
+    footer {
+      background-color: #1f2421;
+      color: white;
+      padding: 10px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    
+    footer p {
+      font-size: 1.3rem;
+      font-weight: 600;
+    }
+    }
